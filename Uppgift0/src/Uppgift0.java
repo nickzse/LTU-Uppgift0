1b0f33e71aecf99a6028196c91c77218c1c2fe69
Nichlas Friman <nzpub@icloud.com>
2021-08-27 09:50:07 +0200

    Initial commit.

---
+++ Uppgift0/src/Uppgift0.java
@@ -0,0 +1,11 @@
+/**
+ * Skriver ut texten "Hej Värld!"
+ * @author Nichlas Friman, nicfri-1
+ */
+public class Uppgift0
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hej Värld!");
+   }
+}
